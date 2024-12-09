274018c7b12ac9891a7130ffdfda6ab749b6bc41
KevsterAmp <kevinamparado13@gmail.com>
2024-12-09 21:27:41 +0800

    upload day 6 part 2; if it works, it works.

--- 2024/day-6/main.go
+++ 2024/day-6/main.go
@@ -15,15 +15,43 @@ func check(e error) {
 }
 
 func main() {
-	// directions := []string{"u", "r", "d", "l"}
-	content, err := os.ReadFile("test.txt")
+	content, err := os.ReadFile("input.txt")
 	check(err)
 	lineStr := strings.Split(string(content), "\n")
 	lineStr = lineStr[:len(lineStr)-1]
+	freshGrid := make([]string, len(lineStr))
+	copy(freshGrid, lineStr)
 	// fmt.Println(string(content))
-	guard := LocateGuard(lineStr)
-	Travel(lineStr, guard)
+	// guard := LocateGuard(lineStr)
+	// total := Travel(lineStr, guard, true)
+	// fmt.Println("cell total: ", total)
+	// PartOne(lineStr)
+	PartTwo(freshGrid)
+}
 
+func PartOne(lineStr []string) {
+	guard := LocateGuard(lineStr)
+	total := Travel(lineStr, guard, true)
+	fmt.Println("cell total: ", total)
+}
+
+func PartTwo(lineStr []string) {
+	counter := 0
+	for y, line := range lineStr {
+		for x, char := range line {
+			if string(char) == "." {
+				freshGrid := make([]string, len(lineStr))
+				copy(freshGrid, lineStr)
+				freshGrid[y] = freshGrid[y][:x] + "#" + freshGrid[y][x+1:]
+				guard := LocateGuard(freshGrid)
+				isLoop := Travel(freshGrid, guard, true)
+				if isLoop == -1 {
+					counter++
+				}
+			}
+		}
+	}
+	fmt.Println("Counter: ", counter)
 }
 
 func LocateGuard(grid []string) []int {
@@ -37,38 +65,60 @@ func LocateGuard(grid []string) []int {
 	return nil
 }
 
-func Travel(grid []string, guard []int) {
-	// var diff int
+func Travel(grid []string, guard []int, checkLoop bool) int {
+	loopCounter := 0
+	var guardHistory [][]int
 	directions := []string{"u", "r", "d", "l"}
 	iter := 0
 	for {
-		if guard == nil {
-			fmt.Println("iter nil: ", iter)
-			// for _, k := range grid {
-			// 	fmt.Println(k)
-			// }
-			total := GetTotalPos(grid)
-			fmt.Println(total)
-			break
-		}
 		for _, d := range directions {
 			x := guard[0]
 			y := guard[1]
 			// fmt.Println("iter: ", iter)
 			// fmt.Println("Guard:", guard)
-			grid[y] = grid[y][:x] + "x" + grid[y][x+1:]
+			grid[y] = grid[y][:x] + "X" + grid[y][x+1:]
 			// for _, k := range grid {
 			// 	fmt.Println(k)
 			// }
 			grid[y] = grid[y][:x] + "." + grid[y][x+1:]
+			guardHistory = append(guardHistory, guard)
 			guard = GetNewPos(grid, guard, d)
 			if guard == nil {
-				break
+				// fmt.Println("iter nil: ", iter)
+				// for _, k := range grid {
+				// 	fmt.Println(k)
+				// }
+				total := GetTotalPos(grid)
+				return total
 			}
-			// fmt.Println("diff: ", diff)
+			if checkLoop && IsLoop(guardHistory, guard) && iter >= 4 {
+				loopCounter++
+				// fmt.Println("iter nil: ", iter)
+				// for _, k := range grid {
+				// 	fmt.Println(k)
+				// }
+			}
+			if loopCounter == 4 {
+				fmt.Println("iter nil: ", iter)
+				for _, k := range grid {
+					fmt.Println(k)
+				}
+				return -1
+			}
+
 			iter++
+
 		}
 	}
+}
+
+func IsLoop(guardHistory [][]int, currentGuard []int) bool {
+	for _, guard := range guardHistory {
+		if currentGuard[0] == guard[0] && currentGuard[1] == guard[1] {
+			return true
+		}
+	}
+	return false
 }
 
 func GetNewPos(grid []string, guard []int, d string) []int {
